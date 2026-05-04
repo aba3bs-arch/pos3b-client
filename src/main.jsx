f5f0922f438557f737e7a306943a168b29b3462e
aba3bs-arch <aba3bs@gmail.com>
2026-05-03 23:51:59 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Esta es la línea que causaba el error si el archivo no existía
+import './index.css' // Esta línea ya no dará error si el archivo existe
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
