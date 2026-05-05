ccbe86e4f1ace8c39741170fa069d2c87c0a72e7
aba3bs-arch <aba3bs@gmail.com>
2026-05-04 22:32:51 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // Esta línea ya no dará error si el archivo existe
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
